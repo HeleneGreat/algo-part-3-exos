a0021e125afe0b8ac9501a9039ee29310bae52f3
Hélène CARRIOU <helenec56@laposte.net>
2021-12-03 10:58:50 +0100

    flizzbuzz BUG

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -9,6 +9,34 @@
  */
 function fizzBuzz(min, max) {
     // CODE HERE
+    // essai en suivant la logique de Song.js :
+    
+  
+    let i = min;
+    let message = [];
+
+
+    while (min <= i <= max){
+        if (i % 3 === 0){
+        message.push("Fizz"); return message}
+        else if (i % 5 === 0) {
+        message.push("Buzz"); return message}
+        else if (i % 3 === 0 || i % 5 === 0) {
+        message.push("FizzBuzz"); return message}
+        else {message.push(i); return message}
+        ++i;
+    }
+
+
+/* 1er essai selon ma logique :
+let fizz = "Fizz";
+let buzz = "Buzz";
+    if (number % 3 === 0) {return fizz;}
+    else if (number % 5 === 0) {return buzz;}
+    else if (number % 3 === 0 || number % 5 === 0) {return fizz + buzz;}
+    else {return number;}
+}
+*/
 }
 
 export { fizzBuzz };
